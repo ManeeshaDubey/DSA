c22d5acb9fce8c4661159ae0a5b9acc053882e13
ManeeshaDubey <maneesha108@gmail.com>
2026-02-16 18:13:56 +0530

    added corner cases for second largest function

--- problems/find_second_largest_number.js
+++ problems/find_second_largest_number.js
@@ -1,19 +1,29 @@
 // Write a function that takes an array of numbers as input and returns the second largest element in the array.
 
 function secondLargestNumber(arr) {
+  if (arr.length < 2) return null;
+
   let largest = arr[0];
   let secondlargest;
+
   for (let i = 1; i < arr.length; i++) {
     if (largest < arr[i]) {
       secondlargest = largest;
       largest = arr[i];
-    } else if (arr[i] < largest && arr[i] > secondlargest) {
+    } else if (arr[i] > secondlargest && arr[i] != largest) {
       secondlargest = arr[i];
     }
   }
   return secondlargest;
 }
 
-let numbers = [10, 5, 20, 18, 20, 14];
+let numbers = [-10, 5, 20, -18, 20, 14];
 let result = secondLargestNumber(numbers);
 console.log(result);
+
+/* Corner Cases handled
+1. Array has duplicates
+2. Array of length less than 2
+3. Negative Numbers
+4. Array is empty
+*/
