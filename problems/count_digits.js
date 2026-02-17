96fec6fe505ccd5b4ff0585519d897acd15c4f5f
ManeeshaDubey <maneesha108@gmail.com>
2026-02-17 23:13:56 +0530

    optimised solution

--- problems/count_digits.js
+++ problems/count_digits.js
@@ -1,13 +1,27 @@
 // Write a function that takes an integer as input and returns the count of digits present in the number.
 
 function countDigits(num) {
-  let number = num.toString();
+  if (num === 0) return 1;
   let count = 0;
-  for (let i = 0; i < number.length; i++) {
+  num = Math.abs(num);
+  while (num > 0) {
+    num = Math.floor(num / 10);
     count++;
   }
   return count;
 }
 
-let result = countDigits(9110360232);
+let result = countDigits(-9110360232);
 console.log(result);
+
+/*
+corner cases
+Handle negative numbers
+To consider 0 has digit and return 1 or not.
+
+Methods
+Math.floor() → rounds down
+Math.ceil() → rounds up
+Math.round() → rounds to nearest integer
+Math.abs() → removes the negative sign
+*/
