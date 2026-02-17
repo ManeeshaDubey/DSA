73eedf9210a72e50304242cc1c7af0d8615a685e
ManeeshaDubey <maneesha108@gmail.com>
2026-02-17 17:24:30 +0530

    right angled triangle pattern

---
+++ Patterns/#7.js
@@ -0,0 +1,27 @@
+/*Pattern – Right - Aligned Triangle
+
+Description: 
+Print a triangle where the stars are aligned to the right.
+This requires printing leading spaces that decrease as the number of stars increases.
+
+Example(n = 5)
+    
+    *
+   **
+  ***
+ ****
+*****
+*/
+
+let n = 5;
+
+for (let i = 0; i < n; i++) {
+  let row = "";
+  for (let j = 0; j < n - (i + 1); j++) {
+    row = row + " ";
+  }
+  for (let k = 0; k <= i; k++) {
+    row = row + "*";
+  }
+  console.log(row);
+}
