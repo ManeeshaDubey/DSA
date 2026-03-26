2438da1ca2d1067d7d9eb1124f312f4395706895
ManeeshaDubey <maneeshadubey108@gmail.com>
2026-03-26 09:10:46 +0530

    Improved version

--- sorting/bubble_sort.js
+++ sorting/bubble_sort.js
@@ -1,21 +1,21 @@
 //Bubble Sort Algorithm
 
 function bubbleSort(arr) {
-  let swapped = false;
   for (let i = 0; i < arr.length - 1; i++) {
+    let swapped = false;
     for (let j = 0; j < arr.length - 1 - i; j++) {
-        if (arr[j] > arr[j + 1]) {
-        swapped = true;
+      if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
-        }
-      if (swapped !== true) return arr;
+        swapped = true;
+      }
     }
+    if (!swapped) break;
   }
   return arr;
 }
 
-let nums = [7,6,5,3,4,2,1];
+let nums = [1, 3, 2];
 let result = bubbleSort(nums);
 console.log(result);
